88d7fbbf7b7c18da5c407add98c630193f90d0bf
Snowly <visionhyronic@gmail.com>
2024-08-29 18:42:35 +0200

    feat: start script to generate vehicles.lua

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {
+    UseSingleResource = true, -- if yes, then it will use the fivem native GetResourcePath.
+    ResourceName = "vehiclemeta" -- resourcename
+    --ResourceName = "[stream]/[vehicles]"  -- also works with UseSingleResource false.
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Project 7 - Snowy'
+description 'Vehicle Meta Data Extractor'
+version '1.0.0'
+
+server_script 'server.lua'
+shared_script 'config.lua'
