0f17a2f069f6be19c318a60c20823dc567d6fb9a
Snowly <visionhyronic@gmail.com>
2024-09-04 09:12:54 +0200

    fix: Config undefined

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,5 @@ author 'Project 7 - Snowy'
 description 'Vehicle Meta Data Extractor'
 version '1.2.0'
 
-server_scripts {
- 'server.lua',
- 'config.lua'
-}    
+server_script 'server.lua'    
+shared_script  'config.lua'
